95cf6adf58db3a98a45f4429ed3ca5b7871c23c5
Sasha <sashajw@umich.edu>
2025-05-14 21:00:27 -0400

    1st

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+//UX FILE - IMMEDIATELY CONTROLS UI
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
