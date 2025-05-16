b3e1d16fc78a79874dd41578165bb22f64bc59ac
Sasha <sashajw@umich.edu>
2025-05-15 22:12:20 -0400

    fixing commit issues

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+//index.tsx is initial route
+
+import { StyleSheet, Text, View } from "react-native";
 //UX FILE - IMMEDIATELY CONTROLS UI
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Time Capsule!</Text>
     </View>
   );
 }
+//edit between text tags to change name
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fdf0d5',
+    borderColor: '#b5838d',
+    borderWidth: 30,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#003049',
+  }
+}); //behaves like css
